fe5f818fc48ece14f7bd99bb668e98f6a7b62de9
Shaharyar Ali <110044492+ShaharyAli30@users.noreply.github.com>
2024-07-01 21:31:35 +0500

    Create Class&Object.cpp

---
+++ Class&Object.cpp
@@ -0,0 +1,33 @@
+
+//Program to add teacher name, subject, dept and salary in oop class and object
+#include <iostream>
+using namespace std;
+
+class Teacher {
+    // Properties
+public:
+    string name;
+    string dept;
+    string subject;
+    float salary;
+
+    // Methods/member function
+    void changedept(string newdept) {
+        dept = newdept;
+    }
+};
+
+int main() {
+    Teacher t1;
+    t1.name = "Ali";
+    t1.changedept("Computer");
+    t1.subject = "C++";
+    t1.salary = 23000;
+
+    cout << "Name: " << t1.name << endl;
+    cout << "Department: " << t1.dept << endl;
+    cout << "Subject: " << t1.subject << endl;
+    cout << "Salary: " << t1.salary << endl;
+
+    return 0;
+}
