b8f60c1959d899a8618c0576cdb5d6b201945408
Shaharyar Ali <110044492+ShaharyAli30@users.noreply.github.com>
2024-08-02 13:48:26 +0500

    Add files via upload

---
+++ Abstraction/Task-1.cpp
@@ -0,0 +1,17 @@
+//Abstraction: It is a process of Hiding unnecessary things and show only important parts.
+// implemented using access modifiers.
+//Abstract class- No obj created in abstract class.
+#include<iostream>
+using namespace std;
+class Abstraction{
+    private:
+    //Hide unimportant and sensitive data;
+
+    public:
+    // show imp parts 
+};
+int main()
+{
+    //No object is create in abstract class only use  to create a blue print of class.
+    return 0;
+}
